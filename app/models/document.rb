0271ab67d9bba5b62f9629282385bcbc150ed148
Ryan Ahearn <ryan.ahearn@gsa.gov>
2024-09-26 14:19:17 -0400

    Use unreleased net/http for https proxy

--- app/models/document.rb
+++ app/models/document.rb
@@ -1,21 +1,28 @@
-require "net/http"
-
 class Document < ApplicationRecord
   validates_presence_of :title, :url
 
   validate :url_is_https
 
   def contents
-    @contents ||= Net::HTTP.start(uri.hostname, uri.port, use_ssl: true) do |request|
+    @contents ||= begin
+      request = Net::HTTP.new(
+        uri.hostname, uri.port,
+        proxy&.hostname, proxy&.port, proxy&.user, proxy&.password, nil, true
+      )
+      request.use_ssl = true
       request.get(uri.path).body
     end
   end
 
-  private
-
   def uri
     @uri ||= URI(url)
   end
+
+  def proxy
+    @proxy_uri ||= URI(ENV["https_proxy"]) if ENV["https_proxy"].present?
+  end
+
+  private
 
   def url_is_https
     parsed = URI(url)
