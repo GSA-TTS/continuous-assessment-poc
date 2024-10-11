fea2dc3dae47ca847d2fa8a67ef1656cf08f89fe
Ryan Ahearn <ryan.ahearn@gsa.gov>
2024-10-11 11:13:32 -0400

    Turn off sidekiq for now

--- terraform/staging/main.tf
+++ terraform/staging/main.tf
@@ -27,15 +27,15 @@ module "database" {
   depends_on    = [module.app_space]
 }
 
-module "redis" {
-  source = "github.com/gsa-tts/terraform-cloudgov//redis?ref=v1.0.0"
+# module "redis" {
+#   source = "github.com/gsa-tts/terraform-cloudgov//redis?ref=v1.0.0"
 
-  cf_org_name     = local.cf_org_name
-  cf_space_name   = local.cf_space_name
-  name            = "${local.app_name}-redis-${local.env}"
-  redis_plan_name = "redis-dev"
-  depends_on      = [module.app_space]
-}
+#   cf_org_name     = local.cf_org_name
+#   cf_space_name   = local.cf_space_name
+#   name            = "${local.app_name}-redis-${local.env}"
+#   redis_plan_name = "redis-dev"
+#   depends_on      = [module.app_space]
+# }
 
 module "egress_space" {
   source = "github.com/gsa-tts/terraform-cloudgov//cg_space?ref=cg-space-asg"
