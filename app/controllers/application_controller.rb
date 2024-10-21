877dd4f6544fa87286ace93f7204aec7d6cb9072
Ryan Ahearn <ryan.ahearn@gsa.gov>
2024-10-21 10:10:56 -0400

    Hidden files should be a warning
    top-level hidden file requests are returning a 200 status, but with the
    content of the home page, not the hidden file. Not ideal, but not dangerous

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,12 @@
 class ApplicationController < ActionController::Base
+  include Pundit::Authorization
+  after_action :verify_pundit_authorization, unless: :devise_controller?
   around_action :switch_locale
 
   def switch_locale(&action)
     locale = params[:locale] || I18n.default_locale
     I18n.with_locale(locale, &action)
   end
-  include Pundit::Authorization
-  after_action :verify_pundit_authorization, unless: :devise_controller?
 
   def verify_pundit_authorization
     if action_name == "index"
