e77a23acdfae5e97a2933ece31e54c114c18ae2e
Ryan Ahearn <ryan.ahearn@gsa.gov>
2024-12-03 09:27:47 -0500

    Migrate egress proxy to new provider

--- terraform/staging/main.tf
+++ terraform/staging/main.tf
@@ -53,16 +53,37 @@ module "egress_space" {
 }
 
 module "egress_proxy" {
-  source = "github.com/gsa-tts/terraform-cloudgov//egress_proxy?ref=v1.1.0"
+  source = "github.com/gsa-tts/terraform-cloudgov//egress_proxy?ref=migrate-provider"
 
-  cf_org_name   = local.cf_org_name
-  cf_space_name = module.egress_space.space_name
-  client_space  = local.cf_space_name
-  name          = "tfm-egress-proxy-${local.env}"
-  # comment out allowlist if this module is being deployed before the app has ever been deployed
-  allowlist = {
-    "${local.app_name}-${local.env}" = ["raw.githubusercontent.com"]
+  cf_org_name = local.cf_org_name
+  cf_egress_space = {
+    id   = module.egress_space.space_id
+    name = module.egress_space.space_name
   }
+  name      = "tfm-egress-proxy-${local.env}"
+  allowlist = ["raw.githubusercontent.com"]
   # depends_on line is needed only for initial creation and destruction. It should be commented out for updates to prevent unwanted cascading effects
   # depends_on = [module.app_space, module.egress_space]
+}
+
+data "cloudfoundry_app" "app" {
+  org_name   = local.cf_org_name
+  space_name = local.cf_space_name
+  name       = "${local.app_name}-${local.env}"
+}
+
+resource "cloudfoundry_network_policy" "egress_policy" {
+  provider = cloudfoundry-community
+  policy {
+    source_app      = data.cloudfoundry_app.app.id
+    destination_app = module.egress_proxy.app_id
+    port            = module.egress_proxy.https_port
+  }
+}
+
+resource "cloudfoundry_service_instance" "egress_proxy_credentials" {
+  name        = "tfm-egress-proxy-${local.env}-creds"
+  space       = module.app_space.space_id
+  type        = "user-provided"
+  credentials = module.egress_proxy.json_credentials
 }
